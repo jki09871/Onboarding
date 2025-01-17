a586fb6a079fdfafccfd8de6374aa19666d7477a
jki09871 <ghdwjdrl56@naver.com>
2025-01-17 20:16:36 +0900

    merge 및 기타 수정

--- src/main/java/com/example/common/ErrorStatus.java
+++ src/main/java/com/example/common/ErrorStatus.java
@@ -19,7 +19,7 @@ public enum ErrorStatus implements BaseCode {
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED,401,"만료된 리프레쉬 토큰입니다."),
     INVALID_REFRESH_TOKEN(HttpStatus.BAD_REQUEST, 400, "잘못된 리프레쉬 토큰입니다"),
     // 유저 관련 예외
-
+    FORBIDDEN_TOKEN(HttpStatus.FORBIDDEN, 403, "관리자 권한이 없습니다."),
     DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, 400, "중복된 닉네임 입니다."),
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, 404, "유저를 찾을 수 없습니다."),
 
@@ -28,7 +28,7 @@ public enum ErrorStatus implements BaseCode {
 
 
     // DB 관련 예외
-    SQL_EXCEPTION_OCCURRED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "데이터베이스 작업 처리 중 예외가 발생했습니다."),
+    SQL_EXCEPTION_OCCURRED(HttpStatus.INTERNAL_SERVER_ERROR, 500, "데이터베이스 작업 처리 중 예외가 발생했습니다.");
 
 
     private final HttpStatus httpStatus;
